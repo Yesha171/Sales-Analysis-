95c04aa8c5cd401f4dc4c763a35328d34ddeeefb
Yesha Patel <yeshapatel171@gmail.com>
2024-11-24 20:22:23 +0000

    Add files via upload

---
+++ SQL_Calendar Query.sql
@@ -0,0 +1,26 @@
+-- Cleansed DimDate Table
+select 
+  [DateKey], 
+  [FullDateAlternateKey] AS Date, 
+  -- [DayNumberOfWeek]
+  [EnglishDayNameOfWeek] AS Day, 
+  --[SpanishDayNameOfWeek]
+  --,[FrenchDayNameOfWeek]
+  -- ,[DayNumberOfMonth]
+  -- ,[DayNumberOfYear]
+  [WeekNumberOfYear] AS WeekNo, 
+  [EnglishMonthName] AS Month, 
+  LEFT([EnglishMonthName], 3) AS MonthShort, 
+  -- ,[SpanishMonthName]
+  -- ,[FrenchMonthName]
+  [MonthNumberOfYear] AS MonthNo, 
+  [CalendarQuarter] AS Quarter, 
+  [CalendarYear] AS Year 
+  --,[CalendarSemester]
+  --,[FiscalQuarter]
+  --,[FiscalYear]
+  --,[FiscalSemester]
+FROM 
+  DimDate
+WHERE
+  CalendarYear >= 2012
